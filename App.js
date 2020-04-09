807484aac85a2e1f06aeed2ad3622d7ec796de2c
Isaac <isaac.smith@joinroot.com>
2020-04-09 14:31:47 -0400

    Added react native shake

--- App.js
+++ App.js
@@ -1,31 +1,41 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import RNShake from "react-native-shake";
 
-export default function App () {
-  const [name, setName] = useState('World');
+export default function App() {
+  const [name, setName] = useState("World");
 
-    return (
-      <View style={styles.container}>
-        <Text style={styles.nameText}>
-          {`Hello, ${name}!`}
-        </Text>
-        <Button color='#4169E1'
-          onPress={() => setName('Katie')}
-          title='Click me'>
-        </Button>
-      </View>
-    );
-  }
+  const handleShake = () => {
+    setName("Isaac");
+  };
+
+  useEffect(() => {
+    RNShake.addEventListener("ShakeEvent", () => handleShake());
+
+    return () => RNShake.removeEventListener("ShakeEvent");
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.nameText}>{`Hello, ${name}!`}</Text>
+      <Button
+        color="#4169E1"
+        onPress={() => setName("Katie")}
+        title="Click me"
+      />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   nameText: {
     fontSize: 50,
     padding: 15,
-  }
+  },
 });
