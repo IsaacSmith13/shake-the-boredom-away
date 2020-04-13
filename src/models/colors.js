eb4b1e45527718c0cbe5a2ca8a4a23440dd73dd0
Isaac <isaac.smith@joinroot.com>
2020-04-13 09:21:22 -0400

    Changed back to colors model file after rebase

--- src/models/colors.js
+++ src/models/colors.js
@@ -1,6 +1,8 @@
 export const Colors = {
-  primary: "#7fcd91",
-  secondary: "#4d4646",
+  accent: "#D24C3B",
   disabled: "#acb5b4",
-  hyperlink: "#00bcd4",
+  hyperlink: "#0000EE",
+  primary: "#EBBC43",
+  secondary: "#f9f6e5",
+  text: "#39373B",
 };
