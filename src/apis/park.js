499edaebd92a2e0a89dfe0d9d13e8e44a9062447
Isaac <isaac.smith@joinroot.com>
2020-04-13 13:27:26 -0400

    Added links to parks

--- src/apis/park.js
+++ src/apis/park.js
@@ -1,23 +1,23 @@
 import { BETTER_ZIP_KEY } from "../../private-keys.js";
 import { getRandomItemFromArr } from "../utils/getRandomItem.js";
-import {getLatLong} from "../utils/getLatLong.js";
 
 export const ParkApi = {
   getRecommendation: async (weather, latLong) => {
     return await fetch(
-      `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?f=json&category=park&location=${latLong.long},${latLong.lat}&outFields=Place_addr,PlaceName&maxLocations=5`
+      `https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?f=json&category=park&location=${latLong.long},${latLong.lat}&outFields=Place_addr,PlaceName&maxLocations=50`
     )
       .then((response) => response.json())
       .then((data) => {
         const park = getRandomItemFromArr(data.candidates);
+
         return {
           title: park.address,
-          header:
-            "Take a stroll in this park!",
-          image: "https://www.portlandoregon.gov/parks/finder/index.cfm?action=ViewFile&PolPhotosID=289",
+          titleLink: `https://www.google.com/maps/@${latLong.lat},${latLong.long},13z`,
+          header: "Take a stroll in this park!",
+          image:
+            "https://www.portlandoregon.gov/parks/finder/index.cfm?action=ViewFile&PolPhotosID=289",
           description: `Current Weather: ${weather.main}`,
-          description2: `Current Temperature: ${weather.temp} ℉`
-
+          description2: `Current Temperature: ${weather.temp} ℉`,
         };
       })
       .catch((err) => {
